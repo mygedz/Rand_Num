a4442623f5f91bd455479c58fcf65568c667d54a
Alexandr Gedz <mygedz@gmail.com>
2019-03-12 22:45:51 +0300

    Изменения в коде программы

--- RandNum/Program.cs
+++ RandNum/Program.cs
@@ -4,14 +4,26 @@ namespace RandNum
 {
     class Program
     {
+        static Random rand = new Random();
         static void Rules()
         {
             //Вывод правил игры
             Console.WriteLine("Правила игры: \nУгадай число, за определенное число попыток. \n\nНастроим условия игры: ");
         }
+        static int AskUser(string message)
+        {
+            Console.WriteLine(message);
+            while (true)
+            {
+                //проверка на число/не число
+                if (int.TryParse(Console.ReadLine(), out int result))
+                    return result;
+
+                Console.WriteLine("Не является числом!");
+            }
+        }
         static int GenerationRandValue(int interval)
         {
-            Random rand = new Random();
             int rndValue = 1 + rand.Next(interval); //генерируем случайно число и записываем его в переменную
             return rndValue; //возвращаем рандомное значение
         }
@@ -23,16 +35,16 @@ namespace RandNum
                 numAttemp++; //счетчик попыток (с какой угадал)
                 attemp--; //счетчик попыток (сколько осталось)
 
-                Console.WriteLine("Попытка " + numAttemp + ". Введите число: ");
+                Console.WriteLine($"Попытка {numAttemp}. Введите число: ");
                 int entNum = Convert.ToInt32(Console.ReadLine()); //запись в переменную числа, которое ввел пользователь
 
                 if (entNum == 0 || entNum > interval || entNum < 0)
                 {
-                    return "Будь внимтельнее! От 1 до " + interval + ".";
+                    return $"Будь внимтельнее! От 1 до {interval}.";
                 }
                 if (entNum == rndValue)
                 {
-                    return "Молодец! Ты угадал с " + numAttemp + " попытки.";
+                    return $"Молодец! Ты угадал с {numAttemp} попытки.";
                 }
             }
             return "Не угадал, это число " + rndValue + ".";
@@ -42,13 +54,10 @@ namespace RandNum
             Console.Title = "Угадай число"; //заголовок окна консоли
             Rules(); //вывод правил
 
-            Console.WriteLine("Выберите промежуток чисел от 1 до ");
-            int interval = Convert.ToInt32(Console.ReadLine()); //запись числа, которое будет промежутком в переменную
+            int interval = AskUser("Выберите промежуток чисел от 1 до "); //переменная для хранения числа промежутка            
+            int attemp = AskUser("Введите количество попыток: "); //переменная для хранения числа количества попыток
+            int rndValue = GenerationRandValue(interval); //переменная для хранения рандомного числа
 
-            Console.WriteLine("Введите количество попыток: ");
-            int attemp = Convert.ToInt32(Console.ReadLine()); //запись числа, которое будет количеством попыток в переменную
-
-            int rndValue = GenerationRandValue(interval); //записываем в переменную rndValue рандомное число, полученное из метода GenerationRandValue
             Console.WriteLine(Game(attemp, rndValue, interval));
             Console.ReadKey();
         }
