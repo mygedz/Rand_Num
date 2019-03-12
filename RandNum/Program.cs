bc05275332ab41be60d45b377aa3703abb79f622
Alexandr Gedz <mygedz@gmail.com>
2019-03-12 16:28:53 +0300

    Доработка программы, до полной работоспособности

--- RandNum/Program.cs
+++ RandNum/Program.cs
@@ -18,31 +18,27 @@ namespace RandNum
             int rndValue = 1 + rand.Next(interval); //генерируем случайно число и записываем его в переменную
             return rndValue; //возвращаем рандомное значени
         }
-
-        static String Game(int attemp, int rndValue, int interval)
+        static string Game(int attemp, int rndValue, int interval)
         {
             int numAttemp = 0; //хранение количества попыток
             while (attemp > 0)
             {
                 numAttemp++; //счетчик попыток (с какой угадал)
                 attemp--; //счетчик попыток (сколько осталось)
+
                 Console.WriteLine("Попытка " + numAttemp + ". Введите число: ");
-                int entNum = Convert.ToInt32(Console.ReadLine()); //запись числа, которое ввел пользователь
+                int entNum = Convert.ToInt32(Console.ReadLine()); //запись в переменную числа, которое ввел пользователь
 
                 if (entNum == 0 || entNum > interval || entNum < 0)
                 {
-                    Console.WriteLine("Будь внимтельнее! От 1 до " + interval + ".");
+                    return "Будь внимтельнее! От 1 до " + interval + ".";
                 }
                 if (entNum == rndValue)
                 {
-                    Console.WriteLine("Молодец! Ты угадал с " + numAttemp + " попыткию");
-                    break;
-                }
-                if (attemp == 0)
-                {
-                    Console.WriteLine("Не угадал, это число " + rndValue + ".");
+                    return "Молодец! Ты угадал с " + numAttemp + " попытки.";
                 }
             }
+            return "Не угадал, это число " + rndValue + ".";
         }
         static void Main(string[] args)
         {
@@ -56,8 +52,7 @@ namespace RandNum
             int attemp = Convert.ToInt32(Console.ReadLine()); //запись числа, которое будет количеством попыток в переменную
 
             int rndValue = GenerationRandValue(interval); //записываем в перменную rndValue рандомное число, полученное из метода GenerationRandValue
-
-            Console.WriteLine();
+            Console.WriteLine(Game(attemp, rndValue, interval));
             Console.ReadKey();
         }
     }
